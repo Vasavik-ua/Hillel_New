a9ae0731e2ff77991d84bfc99777fcf68065c713
vasav <vasyl.documents@yahoo.it>
2023-09-20 22:14:02 +0200

    implement result output

--- Hillel_Vasyl/Home_Work/H.W.15.py
+++ Hillel_Vasyl/Home_Work/H.W.15.py
@@ -24,18 +24,18 @@ def check_isdigit(value):  # check the value and outcome the result.
             new_zero = f'Ви ввели нуль: {new_value}'
             return new_zero
         if value[:1] == '-':
-            result = result + '''Ви ввели від'ємне '''
+            result += '''Ви ввели від'ємне '''
         else:
             result = result + 'Ви ввели позитивне '
         if (value.find('.')) >= 0 or (value.find(',')) >= 0:
-            result = result + 'дробове число: '
+            result += 'дробове число: '
         else:
             result = result + 'ціле число: '
         if value[:1] == '-' and value[1:2] == '.':
-                result = result + (f'-0.{new_value}')
+                result += (f'-0.{new_value}')
                 return result
         elif value[:1] == '.' or value[:1] == ',':
-            result = result + (f'0.{new_value}')
+            result += result (f'0.{new_value}')
             return result
         return result + f'{value}'
     else:
