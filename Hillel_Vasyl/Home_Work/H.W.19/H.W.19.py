16466c2d2e02991c7f53942de7db4632ce80580f
vasav <vasyl.documents@yahoo.it>
2023-09-25 17:47:41 +0200

    improve the h.w.

--- Hillel_Vasyl/Home_Work/H.W.19/H.W.19.py
+++ Hillel_Vasyl/Home_Work/H.W.19/H.W.19.py
@@ -3,20 +3,23 @@ import json
 
 with open('h.w.18.json') as y:
     json_data = json.load(y)
-    print(type(json_data))
-    print(json_data)
-first_row = ["ID", "Name", "Age", "Phone"]
-new_list = list(json_data.items())
-row1 = [new_list[0][0], new_list[0][1][0], new_list[0][1][1]]
-row2 = [new_list[1][0], new_list[1][1][0], new_list[1][1][1]]
-row3 = [new_list[2][0], new_list[2][1][0], new_list[2][1][1]]
-row4 = [new_list[3][0], new_list[3][1][0], new_list[3][1][1]]
-row5 = [new_list[4][0], new_list[4][1][0], new_list[4][1][1]]
+
+
+def cre_new(x):
+    g = list(x.items())
+    w = []
+    for i in (range(len(g))):
+        h = [g[i][0], g[i][1][0], g[i][1][1]]
+        w.append(h)
+    return w
+
+
+first_row = [["ID", "Name", "Age", "Phone"]]
+new_list = cre_new(json_data)
 
 with open('h.m.19.csv', 'w', encoding='utf-8') as f:
     file_new = csv.writer(f, delimiter=',')
-    for item in (first_row, row1, row2, row3, row4, row5):
+    for item in first_row:
         file_new.writerow(item)
-
-
-
+    for item in new_list:
+        file_new.writerow(item)
