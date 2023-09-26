89b1c295369015d07ddaa8f930ac4c41b6d9a99a
vasav <vasyl.documents@yahoo.it>
2023-09-26 20:53:57 +0200

    implement search and delete

--- Hillel_Vasyl/Home_Work/H.W.20/H.W.20.py
+++ Hillel_Vasyl/Home_Work/H.W.20/H.W.20.py
@@ -8,5 +8,21 @@ with open('h.m.19.csv', mode='r', encoding='utf-8') as f:
   for item in reader:
     new_data.append(item)
 
-print(new_data)
+#print(new_data)
 
+def saerch_val(val):  # val = search element.
+  for item in range(len(new_data)):
+    for value in range(len(new_data[item])):
+      if new_data[item][value].lower() == val.lower():
+        return value
+
+
+ y = saerch_val('age')
+
+def delete_serch(val, array_data):
+  for item in array_data:
+    for value in item:
+      delete_items = value.pop(val)
+  return array_data
+
+print(delete_serch())
