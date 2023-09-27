08d089a9ef08f6d62b52fc5488f961229f65d088
vasav <vasyl.documents@yahoo.it>
2023-09-27 16:46:53 +0200

    fixed problem improve sone def.

--- Hillel_Vasyl/Home_Work/H.W.20/H.W.20.py
+++ Hillel_Vasyl/Home_Work/H.W.20/H.W.20.py
@@ -3,23 +3,23 @@ import csv
 
 
 with open('h.m.19.csv', mode='r', encoding='utf-8') as f:
-  reader = csv.reader(f, delimiter=',')
-  new_data = []
-  for item in reader:
-    new_data.append(item)
+    reader = csv.reader(f, delimiter=',')
+    new_data = []
+    for item in reader:
+        new_data.append(item)
 
 
 def search_val(val):  # val = search element.
-  for item in range(len(new_data)):
-    for value in range(len(new_data[item])):
-      if new_data[item][value].lower() == val.lower():
-        return value
+    for item in range(len(new_data)):
+        for value in range(len(new_data[item])):
+            if new_data[item][value].lower() == val.lower():
+                return value
 
 
 def delete_serch(val):
-  for item in new_data:
-    delete_items = item.pop(val)
-  return new_data
+    for item in new_data:
+        item.pop(val)
+    return new_data
 
 
 id_search = (search_val('age'))
@@ -30,35 +30,38 @@ woork_book.create_sheet(title='First sheet', index=0)
 sheet = woork_book['First sheet']
 
 for row_id, row in enumerate(new_array):
-  for col_id, value in enumerate(row):
-    cell = sheet.cell(row=row_id + 1, column=col_id + 1)
-    cell.value = value
+    for col_id, value in enumerate(row):
+        cell = sheet.cell(row=row_id + 1, column=col_id + 1)
+        cell.value = value
 
-woork_book.save('h.w.20.xlsx')
-
-woork_book = openpyxl.load_workbook('h.w.20.xlsx')
-
-sheet = woork_book.active
-print(sheet)
+sheet_1 = woork_book['Sheet']
 
 rows = sheet.max_row
 cols = sheet.max_column
 
-id_new = []
-for item in range(1,2):
-  for val in range(1,rows + 1):
-    cell = sheet.cell(row=val, column=item)
-    id_new.append(cell.value)
+def write_row(sheet,col_n, rows):  # Sheet in use; witch colmn; max val of row.
+    id_new = []
+    for item in range(col_n, col_n+1 ):
+        for val in range(1, rows + 1):
+            cell = sheet.cell(row=val, column=item)
+            id_new.append(cell.value)
+    return id_new
+id_new = write_row(sheet,1,rows)
+#id_new = []
+#for item in range(1, 2):
+#    for val in range(1, rows + 1):
+#        cell = sheet.cell(row=val, column=item)
+#        id_new.append(cell.value)
 name_new = []
-for item in range(2,3):
-  for val in range(1,rows + 1):
-    cell = sheet.cell(row=val, column=item)
-    name_new.append(cell.value)
+for item in range(2, 3):
+    for val in range(1, rows + 1):
+        cell = sheet.cell(row=val, column=item)
+        name_new.append(cell.value)
 phone_new = []
-for item in range(3,4):
-  for val in range(1,rows + 1):
-    cell = sheet.cell(row=val, column=item)
-    phone_new.append(cell.value)
+for item in range(3, 4):
+    for val in range(1, rows + 1):
+        cell = sheet.cell(row=val, column=item)
+        phone_new.append(cell.value)
 
 new_list = []
 new_list.append(id_new)
@@ -67,9 +70,9 @@ new_list.append(phone_new)
 
 
 for row_id, row in enumerate(new_list):
-  for col_id, value in enumerate(row):
-    cell = sheet.cell(row=row_id + 1, column=col_id + 1)
-    cell.value = value
+    for col_id, value in enumerate(row):
+        cell = sheet_1.cell(row=row_id + 1, column=col_id + 1)
+        cell.value = value
 
 woork_book.save('h.w.20+.xlsx')
-
+woork_book = openpyxl.load_workbook('h.w.20+.xlsx')
