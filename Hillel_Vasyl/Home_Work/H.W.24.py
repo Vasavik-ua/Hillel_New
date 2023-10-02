bf3b607b17d58c56afc33a6aa5e7b864cba16f8d
vasav <vasyl.documents@yahoo.it>
2023-10-02 22:19:50 +0200

    implement of sub

--- Hillel_Vasyl/Home_Work/H.W.24.py
+++ Hillel_Vasyl/Home_Work/H.W.24.py
@@ -6,3 +6,22 @@ class Mystr(str):
         return super().__add__(other)
 
 
+    def __sub__(self, other):
+        y = str(self.x)
+        r = str(other.x)
+        w = y.replace(r, '', 1)
+        return w
+
+a = Mystr([1,2,3,4])
+b = Mystr(3)
+c = a - b
+print(type(c))
+print(c)
+
+#tring('New bala7nce') - 7               ->    'New balance'
+#String('New balance') - 'bal'            ->    'New ance'
+#String('New balance') - 'Bal'            ->    'New balance'
+#String('pineapple apple pine') - 'apple' ->    'pine apple pine'
+#String('New balance') - 'apple'          ->    'New balance'
+#String('NoneType') - None                ->    'Type'
+#String(55678345672) - 7                  ->    '5568345672'
