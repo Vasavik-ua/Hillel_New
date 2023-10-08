b621c220e986ea7f3fc4bd5b7a198558f06e55bc
vasav <vasyl.documents@yahoo.it>
2023-10-08 17:34:16 +0200

    implement new code

--- Hillel_Vasyl/Home_Work/Diplom/dyolom_Hillel.py
+++ Hillel_Vasyl/Home_Work/Diplom/dyolom_Hillel.py
@@ -12,47 +12,44 @@ class Person:
     DATE_OF_DEATH = []
     GENDER = []
     AGE = []
+    INIT_TABLE = ['NAME', 'SURNAME', 'SEC_SURNAME',
+                  'DATE_OF_BIRTH', 'DATE_OF_DEATH',
+                  'GENDER', 'AGE']
 
-
+    def init_table(self, shee):
+        for row_id, value in enumerate(Person.INIT_TABLE):
+            cell = shee.cell(row=1, column=row_id + 1)
+            cell.value = value
     def column_name(self, shee, array):
-        array.insert(0, 'NAME: ')
         for row_id, value in enumerate(array):
             cell = shee.cell(row=row_id + 1, column= 1)
             cell.value = value
     def column_surname(self, shee, array):
-        array.insert(0, 'SURNAME: ')
         for row_id, value in enumerate(array):
             cell = shee.cell(row=row_id + 1, column= 2)
             cell.value = value
     def column_sec_surname(self, shee, array):
-        array.insert(0, 'SEC SURNAME: ')
         for row_id, value in enumerate(array):
             cell = shee.cell(row=row_id + 1, column= 3)
             cell.value = value
     def column_birth(self, shee, array):
-        array.insert(0, 'DATE OF BIRTH: ')
         for row_id, value in enumerate(array):
             cell = shee.cell(row=row_id + 1, column= 4)
             cell.value = value
     def column_death(self, shee, array):
-        array.insert(0, 'DATE OF DEATH: ')
         for row_id, value in enumerate(array):
             cell = shee.cell(row=row_id + 1, column= 5)
             cell.value = value
 
     def column_gender(self, shee, array):
-        array.insert(0, 'GENDER: ')
         for row_id, value in enumerate(array):
             cell = shee.cell(row=row_id + 1, column= 6)
             cell.value = value
 
     def column_age(self, shee, array):
-        array.insert(0, 'AGE: ')
         for row_id, value in enumerate(array):
             cell = shee.cell(row=row_id + 1, column= 7)
             cell.value = value
-
-
 
 
 
@@ -65,7 +62,6 @@ def load_button():
     Person.GENDER.append(text_gender.get())
     Person.AGE.append(check_age(Person.DATE_OF_BIRTH,
                                 Person.DATE_OF_DEATH))
-
 
     text_name.delete(0, END)
     text_surname.delete(0, END)
